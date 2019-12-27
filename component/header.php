8f74664f9e6003cbfe3859f578189445ed0858d7
yharuko <56985853+yharuko@users.noreply.github.com>
2019-12-27 14:25:09 +0900

    Add files via upload

---
+++ component/header.php
@@ -0,0 +1,32 @@
+<header>
+    <div class="container" id="header">
+        <nav class="navbar navbar-expand-md navbar-light bg-white">        
+          <a class="navbar-brand" href="#">
+            <img src="img/shoplogo.jpg" class="img-fluid mr-5 ml-2">
+          </a>
+            <button type="button" class="navbar-toggler collapsed" data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="ナビゲーションの切替">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+        
+          <div class="collapse navbar-collapse" id="Navber">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link btn-success text-white" href="https://plantage.theshop.jp" target="_blank">ショップサイト<span class="sr-only">(現位置)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://plantage.theshop.jp/categories/1522645" target="_blank">ハーブブレンドティー</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://plantage.theshop.jp/categories/1639982" target="_blank">ブレンド薬膳茶</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://plantage.theshop.jp/categories/1399295" target="_blank">ハーブ・スパイス</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://plantage.theshop.jp/categories/1399294" target="_blank">薬膳食材</a>
+              </li>
+            </ul>
+          </div>
+        </nav>        
+    </div>
+  </header>
---
+++ component/top_image.php
@@ -0,0 +1,11 @@
+<div class="container">
+  <div class="d-flex justify-content-center">
+    <div class="row">
+      <div class="col-sm-12 col-md-12">
+        <div class="card">
+        <img src="img/LPキャッチコピー1-min_s.png" class="card-img">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
